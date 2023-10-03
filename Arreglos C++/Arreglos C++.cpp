adf9f9fffd8e6be41b6a7e53eb1c5eb833ca1a82
Amin070204 <145484915+Amin070204@users.noreply.github.com>
2023-10-03 09:01:08 -0600

    Agregar archivos de proyecto.

---
+++ Arreglos C++/Arreglos C++.cpp
@@ -0,0 +1,20 @@
+// Arreglos C++.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int numeros[6] = { 10,20,30,40,50,60 };
+	int n[3] = { 3,4,5 };
+	char c[4] = { 'L','U','I','S' };
+	char s[] = "Mona Lisa";
+	cout << numeros[1] << endl;
+	cout << n[0] << endl;
+	cout << "\n";
+	cout << c << endl;
+	cout << "\n";
+	cout << s << endl;
+	system("pause");
+	return 0;
+}
